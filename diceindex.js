a4a8dfb47c54c063bb0cac91355d1618edec142c
JakeBevan <63398348+JakeBevan@users.noreply.github.com>
2020-05-01 10:43:29 +0100

    force github to change javascript file

---
+++ diceindex.js
@@ -0,0 +1,22 @@
+function Play() {
+
+var randomNumber1 = Math.ceil((Math.random()) * 6);
+
+document.querySelector(".img1").setAttribute("src", "Images/dice" + randomNumber1 + ".png");
+
+var randomNumber2 = Math.ceil((Math.random()) * 6);
+
+document.querySelector(".img2").setAttribute("src", "Images/dice" + randomNumber2 + ".png");
+
+if (randomNumber1 === randomNumber2) {
+    document.querySelector(".title").innerText = "Draw!";
+}
+  else if (randomNumber1 > randomNumber2) {
+    document.querySelector(".title").innerText = "🚩  Player 1 Wins!";
+  }
+
+  else {
+    document.querySelector(".title").innerText = "Player 2 Wins!  🚩";
+  }
+
+}
